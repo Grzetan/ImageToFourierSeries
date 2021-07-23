30b67554d1b6b123709214538b39de453034f94a
Grzetan <grzetan@gmail.com>
2021-07-23 17:19:07 +0200

    Added gradient intensity calculation

--- src/image_to_path.py
+++ src/image_to_path.py
@@ -1,50 +1,98 @@
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageFilter
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-img = ImageOps.grayscale(Image.open("l.png"))
+img = Image.open("face.jpeg")
+img = ImageOps.grayscale(img)
 img = np.array(img)
-threshold = 30
 
-feature_map = np.full(img.shape, 0)
+def generate_gaussian_kernel(kernel_size, sigma):
+    ax = np.linspace(-(kernel_size - 1) / 2, (kernel_size - 1) / 2, kernel_size)
+    x,y =  np.meshgrid(ax, ax)
+    kernel = np.exp(-0.5 * (np.square(x) + np.square(y)) / np.square(sigma))
+    return kernel / np.sum(kernel)
 
-pixels_around = [
-    (-1, -1), (0, -1), (1, -1),
-    (-1, 0),           (1, 0),
-    (-1, 1),  (0, 1),  (1, 1)
-]
+def convolve(img, kernel):
+    kernel_size = kernel.shape[0]
+    pad_size = (kernel_size - 1) / 2
+    gradient_map = np.zeros(img.shape)
+    img = np.pad(img, int(pad_size))
 
-horizontal_kernel = [
-    -1, 0, 1,
-    -2,    2,
-    -1, 0, 1
-]
+    for y in range(gradient_map.shape[0]):
+        for x in range(gradient_map.shape[1]):
+            gradient_map[y,x] = np.sum(img[y:y+kernel_size, x:x+kernel_size] * kernel)
 
-vertical_kernel = [
-    -1, -2, -1,
-     0,      0,
-     1,  2,  1
-]
+    return gradient_map
 
-# Create feature map
-for y in range(1, img.shape[0] - 1):
-    for x in range(1, img.shape[1] - 1):
+# Apply gaussian blur
+kernel = generate_gaussian_kernel(5, 2)
+img = convolve(img, kernel)
 
-        horizontal_kernel_value = 0
-        vertical_kernel_value = 0
+# Find gradients
+x_kernel = np.array([
+    [-1,0,1],
+    [-2,0,2],
+    [-1,0,1]
+])
 
-        for i, pixel in enumerate(pixels_around):
-            value_of_pixel = img[y + pixel[1], x + pixel[0]]
-            horizontal_kernel_value += value_of_pixel * horizontal_kernel[i]
-            vertical_kernel_value += value_of_pixel * vertical_kernel[i]
+y_kernel = np.array([
+    [-1,-2,-1],
+    [0,0,0],
+    [1,2,1]
+])
 
-        kernel_value = math.sqrt(math.pow(horizontal_kernel_value, 2) + math.pow(vertical_kernel_value, 2))
-        kernel_value = 255 if kernel_value > threshold else 0
-        feature_map[y, x] = kernel_value
+g_x = convolve(img, x_kernel)
+g_y = convolve(img, y_kernel)
 
-print(np.count_nonzero(feature_map == 255))
-plt.imshow(feature_map)
-plt.show()
+gradients = np.sqrt(np.square(g_x) + np.square(g_y))
+gradients = gradients / gradients.max() * 255
+theta = np.arctan2(g_y, g_x)
+
+img = Image.fromarray(gradients)
+img.show()
+
+
+
+#
+#
+# feature_map = np.full((img.shape[0], img.shape[1], 2), 0)
+#
+# pixels_around = [
+#     (-1, -1), (0, -1), (1, -1),
+#     (-1, 0),           (1, 0),
+#     (-1, 1),  (0, 1),  (1, 1)
+# ]
+#
+# horizontal_kernel = [
+#     -1, 0, 1,
+#     -1,    1,
+#     -1, 0, 1
+# ]
+#
+# vertical_kernel = [
+#     -1, -1, -1,
+#      0,      0,
+#      1,  1,  1
+# ]
+#
+# # Create feature map
+# for y in range(1, img.shape[0] - 1):
+#     for x in range(1, img.shape[1] - 1):
+#
+#         horizontal_kernel_value = 0
+#         vertical_kernel_value = 0
+#
+#         for i, pixel in enumerate(pixels_around):
+#             value_of_pixel = img[y + pixel[1], x + pixel[0]]
+#             horizontal_kernel_value += value_of_pixel * horizontal_kernel[i]
+#             vertical_kernel_value += value_of_pixel * vertical_kernel[i]
+#
+#         gradient = math.sqrt(math.pow(horizontal_kernel_value, 2) + math.pow(vertical_kernel_value, 2))
+#         feature_map[y, x, 0] = gradient
+#         feature_map[y, x, 1] = math.atan2(vertical_kernel_value, horizontal_kernel_value)
+#         print(feature_map[y,x,1])
+
+
 # TODO
 # https://www.analyticsvidhya.com/blog/2021/03/edge-detection-extracting-the-edges-from-an-image/
